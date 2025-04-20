25bc3dee9e5520181bd6ae46c5499ed048973062
khashayar <aripastudio44@gmail.com>
2025-04-19 22:18:37 -0700

    commit

--- ChangeDNSwindows/RLs.d
+++ ChangeDNSwindows/RLs.d
@@ -3,23 +3,28 @@ module RLs;
 import std.stdio;
 import SaveMangaerInterface;
 import GLV;
+import core.sys.windows.windows;
 import std.string;
-
 
 class CRLs
 {
-	 private auto InterFaceMangaer = new SaveMangerInterFaceClass();
+    private SaveMangerInterFaceClass InterFaceMangaer; 
+    this()
+    {
+        InterFaceMangaer = new SaveMangerInterFaceClass();
+    }
 
-	void AddInterFace(string NameInterface)
-	{
-		writeln("Please Enter Your InterFace(Save in Software)");
-		string InterFace = readln().strip();
-		if(!InterFace.empty)
-		{
-			
-		}else
-		{
-			writeln("Error : Input is empty");
-		}
-	}
+    void AddInterFace(string NameInterface)
+    {
+        writeln("Please Enter Your InterFace(Save in Software)");
+        string InterFace = readln().strip();
+        if (!InterFace.empty)
+        {
+            // ...
+        }
+        else
+        {
+            writeln("Error : Input is empty");
+        }
+    }
 }
