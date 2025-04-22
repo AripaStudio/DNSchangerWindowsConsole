ed70279a9e6c3320564e95f365bb9b8e9c0e88bb
khashayar <aripastudio44@gmail.com>
2025-04-21 21:03:36 -0700

    Fix Bug SelectedData start V1.5.0

--- ChangeDNSwindows/SaveMangaerInterface.d
+++ ChangeDNSwindows/SaveMangaerInterface.d
@@ -12,33 +12,28 @@ import GLV;
 import std.array;
 import std.typecons : Tuple;
 
+
+
 public class JsonDataFileManager
 {
-	public string NameInterFace;
-	
-	//public string Lang;
-	
+    public string NameInterFace;
 
-	JSONValue toJson()
-	{
-		return JSONValue([
-		"NameInterFace" : JSONValue(NameInterFace)
-		
+    JSONValue toJson()
+    {
+        return JSONValue([
+            "NameInterFace": JSONValue(NameInterFace)
+			]);
+    }
 
-		]);
-	}
-
-	static JsonDataFileManager fromJson(JSONValue json)
-	{
-		auto result = new JsonDataFileManager();
-		if("NameInterFace" in json)
-		{
-			result.NameInterFace = json["NameInterFace"].str;			
-		}
-		
-		return result;
-	}
-
+    static JsonDataFileManager fromJson(JSONValue json)
+    {
+        auto result = new JsonDataFileManager();
+        if ("NameInterFace" in json)
+        {
+            result.NameInterFace = json["NameInterFace"].str;
+        }
+        return result;
+    }
 }
 
 public class SaveMangerInterFaceClass
@@ -50,7 +45,9 @@ public class SaveMangerInterFaceClass
 
 	string GetSelectedInterface()
     {
-        return selectedInterface;
+
+		return selectedInterface;		
+
     }
 
 	this()
@@ -67,20 +64,49 @@ public class SaveMangerInterFaceClass
 			{
 				string JsonString = readText(fileName);
 				JSONValue json = parseJSON(JsonString);
-				ManagerData = json.array.map!(j => JsonDataFileManager.fromJson(j)).array;
+				if(json.type != JSONType.object)
+				{
+					writeln(GLVclass.tRED, "Error: JSON root is not an object!", GLVclass.tRESET);
+                    ManagerData = [];
+                    selectedInterface = null;
+                    return;
+				}
+				if("Interfaces" in json && json["Interfaces"].type == JSONType.array)
+				{
+					ManagerData = json["Interfaces"].array.map!(j => JsonDataFileManager.fromJson(j)).array;
+				}else
+				{
+					ManagerData = [];
+				}
+
+				if("SelectedInterFace" in json && json["SelectedInterFace"].type == JSONType.string)
+                {
+                    selectedInterface = json["SelectedInterFace"].str;
+                }
+                else
+                {
+                    selectedInterface = null;
+                }
+				
 			}catch(JSONException e)
 			{
-				writeln( GLVclass.tRED ,"Error parsing JSON file: ", e.msg , GLVclass.tRESET);
+				writeln(GLVclass.tRED, "Error parsing JSON file: ", e.msg, GLVclass.tRESET);
+                ManagerData = [];
+                selectedInterface = null;
 			}
 		}else
 		{
 			ManagerData = [];
+			selectedInterface = null;
+			saveData();
 		}
 	}
 
 	private void saveData()
 	{
-		JSONValue json = JSONValue(ManagerData.map!(c => c.toJson()).array);
+		JSONValue json ;
+		json["Interfaces"]= JSONValue(ManagerData.map!(c => c.toJson()).array);
+		json["SelectedInterFace"] = JSONValue(selectedInterface);		
 		std.file.write(fileName , json.toPrettyString());
 	}
 
@@ -107,52 +133,7 @@ public class SaveMangerInterFaceClass
 		writeln("Name InterFace : " , NameInterface);
 	}
 
-	void DeleteInterFace(string NameInterface)
-	{
-		if(NameInterface.empty)
-		{
-			writeln("Error , Name InterFace is empty");
-			return;
-		}
-		auto existingData = ManagerData.filter!(c => c.NameInterFace == NameInterface).array;
-		if(existingData.empty)
-		{
-			writeln("Error: No NameInterFace found with Name InterFace : ", NameInterface);
-			return;
-		}
 
-		ManagerData = ManagerData.filter!(c => c.NameInterFace != NameInterface).array;
-
-
-		saveData();
-		writeln(NameInterface , " deleted successfully!");
-	}
-
-
-	void ShowAll()
-	{
-		if(ManagerData.empty)
-		{
-			writeln("No Data found!");
-            return;
-		}
-
-		foreach(Data; ManagerData)
-		{
-			writeln("Name InterFace  :  ", Data.NameInterFace);			
-			writeln(GLVclass.tBLUE , "-----------" , GLVclass.tRESET);			
-		}
-		if(selectedInterface.empty)
-		{
-			writeln(GLVclass.tBLUE, "No Interface selected.", GLVclass.tRESET);
-        }
-        else
-        {
-            writeln(GLVclass.tBLUE, "Selected Interface: ", selectedInterface, GLVclass.tRESET);
-        }
-		
-		
-	}
 
 	void SelectInterface(string selectedInterface)
     {
@@ -170,10 +151,72 @@ public class SaveMangerInterFaceClass
             return;
         }
 
-        this.selectedInterface = selectedInterface;
-        writeln("Interface selected successfully");
-        writeln("Selected Interface: ", selectedInterface);
+		this.selectedInterface = selectedInterface;
+        try
+        {
+            saveData();
+            writeln("Interface selected successfully");
+            writeln("Selected Interface: ", selectedInterface);
+        }
+        catch (Exception e)
+        {
+            writeln("Error saving data: ", e.msg);
+        }
     }
+
+
+
+	void DeleteInterFace(string NameInterface)
+	{
+		if(NameInterface.empty)
+		{
+			writeln("Error , Name InterFace is empty");
+			return;
+		}
+		auto existingData = ManagerData.filter!(c => c.NameInterFace == NameInterface).array;
+		if(existingData.empty)
+		{
+			writeln("Error: No NameInterFace found with Name InterFace : ", NameInterface);
+			return;
+		}
+
+		ManagerData = ManagerData.filter!(c => c.NameInterFace != NameInterface).array;
+		if(selectedInterface == NameInterface)
+		{
+			selectedInterface = null;
+		}
+
+		saveData();
+		writeln(NameInterface , " deleted successfully!");
+	}
+
+
+	void ShowAll()
+	{
+		if (ManagerData.empty)
+        {
+            writeln("No Data found!");
+            return;
+        }
+
+        foreach (Data; ManagerData)
+        {
+            writeln("Name InterFace: ", Data.NameInterFace);
+            writeln(GLVclass.tBLUE, "-----------", GLVclass.tRESET);
+        }
+
+        if (selectedInterface is null || selectedInterface.empty)
+        {
+            writeln(GLVclass.tBLUE, "No Interface selected.", GLVclass.tRESET);
+        }
+        else
+        {
+            writeln(GLVclass.tBLUE, "Selected Interface: ", selectedInterface, GLVclass.tRESET);
+        }
+		
+		
+	}
+
 
 
 }
